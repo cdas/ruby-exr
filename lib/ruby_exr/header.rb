67580440022778305857475ceee26057e72b3299
Sebastian Thiel <byronimo@gmail.com>
2012-03-08 23:24:02 +0100

    implemented a few methods (docs are still partly missing), and wrote a tiny test which is to be completed as well. However, it impressively shows that it is already on a good way

--- lib/ruby_exr/header.rb
+++ lib/ruby_exr/header.rb
@@ -2,6 +2,30 @@
 
 module RubyEXR
 	class Header
+		# initialize ourself with an optional file stream
+		def initialize(stream=nil)
+			@header = Hash.new
+			if stream
+				read stream
+			end
+		end
+		
+		# Interface
+		def attribute(name)
+			attribute_info name[1]
+		end
+		
+		# Returns a list of [attr_type_name, data] for the given attribute name
+		# Nil is returned if the attribute doesn't exist in the header.
+		def attribute_info(name)
+			@header[name]
+		end
+		
+		def attributes
+			@header.keys
+		end
+		
+		# end interface
 		
 		
 	end # class header
--- test/exr_header_test.rb
+++ test/exr_header_test.rb
@@ -3,8 +3,12 @@ require File.join(File.dirname(__FILE__), 'base')
 
 class TestExrHeader < TestBase
 	
-	def test_load
-		assert(false, 'is this a comment?')
+	def test_initialization
+		h = RubyEXR::Header.new
+		
+		# verify an empty, uninitialized instance throws proper exceptions
+		assert(h.attribute_info(:nonexisting) == nil, "should get nil for nonexisting attr")
+		assert(h.attributes.size == 0, "should have no attributes yet")
 	end
 	
 end #
