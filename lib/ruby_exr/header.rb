c50c84bddfe496d2dbdc330ffda85fc05f4b1485
Sebastian Thiel <byronimo@gmail.com>
2012-03-15 23:53:41 +0100

    implemented channel including test

--- lib/ruby_exr/header.rb
+++ lib/ruby_exr/header.rb
@@ -1,6 +1,77 @@
 
 
 module RubyEXR
+		
+		
+	MAGIC = 0x01312f76
+	COMPRESSION = ['NO','RLE','ZIPS','ZIP','PIZ','PXR24','B44','B44A']
+	LINEORDER = ['INCRESING Y','DECREASING Y','RANDOM Y']
+	PIXELTYPE = ['UINT','HALF','FLOAT']
+	
+	COMPRESSION_NONE = 0
+	COMPRESSION_RLE = 1
+	COMPRESSION_ZIPS = 2
+	COMPRESSION_ZIP = 3
+	COMPRESSION_PIZ = 4
+	COMPRESSION_PXR24 = 5
+	COMPRESSION_B44 = 6
+	COMPRESSION_B44A = 7
+	
+	LINEORDER_INCREASING_Y = 0
+	LINEORDER_DECREASING_Y = 1
+	LINEORDER_RANDOM_Y = 2
+	
+	PIXELTYPE_UINT = 0
+	PIXELTYPE_HALF = 1
+	PIXELTYPE_FLOAT = 2
+	
+	class Channel
+		@@dot = '.'
+		
+		def initialize(name='', type=PIXELTYPE_HALF, x_sampling=1, y_sampling=1, p_linear=false)
+			@name = name
+			@type = type
+			@x_sampling = x_sampling
+			@y_sampling = y_sampling
+			@p_linear = p_linear
+		end
+		
+		attr_reader :name
+		attr_reader :type
+		attr_reader :x_sampling
+		attr_reader :y_sampling
+		attr_reader :p_linear
+		
+		def to_s
+			"Channel(#{@name}, #{PIXELTYPE[@type]}, #{@x_sampling}, #{@y_sampling}, #{@p_linear})"
+		end
+		
+		def == (other)
+			@name == other.name
+		end
+		
+		def compatible?(rhs)
+			@type == rhs.type && 
+			@x_sampling == rhs.x_sampling &&
+			@y_sampling == rhs.y_sampling &&
+			@p_linear == rhs.p_linear
+		end
+		
+		def layer
+			i = @name.rindex @@dot
+			return @name unless i
+			@name[0...i]
+		end
+		
+		def suffix
+			i = @name.rindex @@dot
+			return "" unless i
+			@name[i+1..-1]
+		end
+		
+		
+	end
+	
 	class Header
 		# initialize ourself with an optional file stream
 		def initialize(stream=nil)
@@ -9,6 +80,8 @@ module RubyEXR
 				read stream
 			end
 		end
+		
+		
 		
 		# Interface
 		def attribute(name)
--- test/exr_header_test.rb
+++ test/exr_header_test.rb
@@ -11,4 +11,25 @@ class TestExrHeader < TestBase
 		assert(h.attributes.size == 0, "should have no attributes yet")
 	end
 	
+	def test_channel
+		c = RubyEXR::Channel.new
+		assert ! c != c
+		
+		assert c.compatible? c
+		assert c.to_s == "Channel(, HALF, 1, 1, false)"
+		assert c == RubyEXR::Channel.new
+		
+		#Layer examples
+		[['layer', 'layer', ''],
+		 ['lay.g', 'lay',   'g'],
+		 ['m.l.b', 'm.l',   'b'],
+		 ['.r',    '',      'r'],
+		 [''     , ''   ,   '' ]].each do |name, layer, suffix|
+		 	 c = RubyEXR::Channel.new(name)
+		 	 assert c.layer == layer
+		 	 assert c.suffix == suffix
+		 end
+		 
+	 end # test_channel
+	
 end #
